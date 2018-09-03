a7f1a9384e210bca7ed9b52d4d9f90f19ce0e825
hcx07 <756170593@qq.com>
2018-09-03 11:32:34 +0800

    234

---
+++ src/Ford/Escape/Escape2013.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by muniao.
+ * User: muniao
+ * Date: 2018/9/3
+ * Time: 11:22
+ */
+namespace Ford\Escape;
+
+class Escape2013
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2013!<br />";
+    }
+}
